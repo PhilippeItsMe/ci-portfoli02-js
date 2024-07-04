a9cb8fb3669fe36f428ad24e14d7654c05635976
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-07-04 16:18:18 +0000

    Change in questions wording

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ let q2 = ["Question 2/6 : What is Jimmy Page best known band?", "Led Zepplin", "
 let q3 = ["Question 3/6 : For what is Steve Vai renowned in the 80s ?", "Slide Guitar", "Floyd Rose Use", "Blues", "Floyd Rose Use", "q4"];
 let q4 = ["Question 4/6 : How did Jimmy Hendrix played sometime his guitar ?", "With His Feet", "With His Elbow", "With His Teeth", "With His Teeth", "q5"];
 let q5 = ["Question 5/6 : What is Tony Iommi, the guitarist of Black Sabbath, famous for?", "His Aircut", "Distortion", "Heavy Riffs", "Heavy Riffs", "q6"];
-let q6 = ["Question 6/6 : What musical genre is Yngwie Malmsteen credited with ?", "Neo Metal", "Blues", "Rock", "Neo Metal", "q7"];
-let q7 = ["You know now if you fully lived the 80' and if you are a rock dude!", "Start again", "Start again", "Start again", "Start again", "q7"];
+let q6 = ["Question 6/6 : What musical genre is Yngwie Malmsteen credited with ?", "Neoclassical Metal", "Blues", "Rock", "Neoclassical Metal", "q7"];
+let q7 = ["You know now if you fully lived the 80' and if you are a rock dude!", "Game Over", "Game Over", "Game Over", "Game Over", "q7"];
 
 function displayQuestion() {
 
