354a105083c74e6535ff9b68814194cb517c2371
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-07-04 16:08:23 +0000

    Add questions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+let q1 = ["Question 1/6 : What innovative technique is Eddie Van Halen famous for ?", "Slide guitar", "Tapping", "Fingerpicking", "Tapping", "q2"];
+let q2 = ["Question 2/6 : What is Jimmy Page best known band?", "Led Zepplin", "The Who", "Kiss", "Led Zepplin", "q3"];
+let q3 = ["Question 3/6 : For what is Steve Vai renowned in the 80s ?", "Slide Guitar", "Floyd Rose Use", "Blues", "Floyd Rose Use", "q4"];
+let q4 = ["Question 4/6 : How did Jimmy Hendrix played sometime his guitar ?", "With His Feet", "With His Elbow", "With His Teeth", "With His Teeth", "q5"];
+let q5 = ["Question 5/6 : What is Tony Iommi, the guitarist of Black Sabbath, famous for?", "His Aircut", "Distortion", "Heavy Riffs", "Heavy Riffs", "q6"];
+let q6 = ["Question 6/6 : What musical genre is Yngwie Malmsteen credited with ?", "Neo Metal", "Blues", "Rock", "Neo Metal", "q7"];
+let q7 = ["You know now if you fully lived the 80' and if you are a rock dude!", "Start again", "Start again", "Start again", "Start again", "q7"];
+
+function displayQuestion() {
+
+};
+
+function getAnswer() {
+
+};
+
+function checkAnswer() {
+
+};
+
+function incrementGoodAnswer() {
+
+};
+
+function incrementBadAnswer() {
+
+};
+
+function updateYouAre() {
+
+};
