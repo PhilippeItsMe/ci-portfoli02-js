797103cd84b6e322e990afedd4891391a7fca583
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-07-09 13:42:21 +0000

    Add getAnwer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-let q1 = ["Question 1/6 : What innovative technique is Eddie Van Halen famous for ?", "Slide guitar", "Tapping", "Fingerpicking", "Tapping", "q2"];
-let q2 = ["Question 2/6 : What is Jimmy Page best known band?", "Led Zepplin", "The Who", "Kiss", "Led Zepplin", "q3"];
-let q3 = ["Question 3/6 : For what is Steve Vai renowned in the 80s ?", "Slide Guitar", "Floyd Rose Use", "Blues", "Floyd Rose Use", "q4"];
-let q4 = ["Question 4/6 : How did Jimmy Hendrix played sometime his guitar ?", "With His Feet", "With His Elbow", "With His Teeth", "With His Teeth", "q5"];
-let q5 = ["Question 5/6 : What is Tony Iommi, the guitarist of Black Sabbath, famous for?", "His Aircut", "Distortion", "Heavy Riffs", "Heavy Riffs", "q6"];
-let q6 = ["Question 6/6 : What musical genre is Yngwie Malmsteen credited with ?", "Neoclassical Metal", "Blues", "Rock", "Neoclassical Metal", "q7"];
-let q7 = ["You know now if you fully lived the 80' and if you are a rock dude!", "Game Over", "Game Over", "Game Over", "Game Over", "q7"];
+// To list all questions in an array
 
-// To watch and react to the mouse going over the three answer buttons
+const questions = [
+    ["Question 1/6 : What innovative technique is Eddie Van Halen famous for ?", "Slide guitar", "Tapping", "Fingerpicking", "Tapping", "q2"],
+    ["Question 2/6 : What is Jimmy Page best known band?", "Led Zepplin", "The Who", "Kiss", "Led Zepplin", "q3"],
+    ["Question 3/6 : For what is Steve Vai renowned in the 80s ?", "Slide Guitar", "Floyd Rose Use", "Blues", "Floyd Rose Use", "q4"],
+    ["Question 4/6 : How did Jimmy Hendrix played sometime his guitar ?", "With His Feet", "With His Elbow", "With His Teeth", "With His Teeth", "q5"],
+    ["Question 5/6 : What is Tony Iommi, the guitarist of Black Sabbath, famous for?", "His Aircut", "Distortion", "Heavy Riffs", "Heavy Riffs", "q6"],
+    ["Question 6/6 : What musical genre is Yngwie Malmsteen credited with ?", "Neoclassical Metal", "Blues", "Rock", "Neoclassical Metal", "over"]
+];
+
+// To react to the mouse going over the answer buttons
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -21,25 +24,36 @@ document.addEventListener("DOMContentLoaded", function () {
             button.style.background = "white"
         });
     }
+    getAnswer();
 });
 
-
-
-
-
-
-function answerAnimation() {
-    let buttonStyle = windows.getComputedStyle("button");
-    console.log(buttonStyle);
-}
-
+// To capture the user answer when his mouse is clicking on an answer button
 
 function getAnswer() {
 
+    let buttons = document.getElementsByTagName('button');
+
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        button.addEventListener('click', () => {
+            let answer = this.textContent;
+            checkAnswer(answer);
+        })
+    };
 };
+
+//To check if the answer is correct
 
 function checkAnswer() {
 
+    let questionDomText = document.getElementById('questiontext');
+
+    for (let i = 0; i < questions.length; i++) {
+        let question = questions[i];
+        let questionText = question[0];
+
+
+    };
 };
 
 function incrementGoodAnswer() {
